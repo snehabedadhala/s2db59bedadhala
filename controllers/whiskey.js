59f16b552b3934d1da108eadd0617de3d0baac2c
snehabedadhala <snehareddy2529@gmail.com>
2022-04-14 22:45:27 -0500

    Resource update

--- controllers/whiskey.js
+++ controllers/whiskey.js
@@ -66,3 +66,35 @@ exports.whiskey_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+// for a specific Whiskey. 
+exports.whiskey_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await whiskey.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
+// Handle Costume update form on PUT. 
+exports.whiskey_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await whiskey.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.name) toUpdate.name = req.body.name; 
+        if(req.body.whiskey_type) toUpdate.whiskey_type = req.body.whiskey_type; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
