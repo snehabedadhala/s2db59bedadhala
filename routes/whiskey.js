6e809b92201aa7dfb49e5cb51644c3bc01dbed24
snehabedadhala <snehareddy2529@gmail.com>
2022-04-20 16:20:03 -0500

    Added Delete Page

--- routes/whiskey.js
+++ routes/whiskey.js
@@ -11,3 +11,9 @@ router.get('/detail', whiskey_controlers.whiskey_view_one_Page);
 
 /* GET create costume page */ 
 router.get('/create', whiskey_controlers.whiskey_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', whiskey_controlers.whiskey_update_Page); 
+
+/* GET delete costume page */ 
+router.get('/delete', whiskey_controlers.whiskey_delete_Page); 
