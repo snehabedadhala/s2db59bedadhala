06f4ae37bb20fcf45a8cfd2d508d95d3861645c0
snehabedadhala <snehareddy2529@gmail.com>
2022-04-26 17:23:44 -0500

    Added Authentication

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }));
+
+
+const connectionString =  process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  {useNewUrlParser: true, useUnifiedTopology: true});
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+console.log("Connection to DB succeeded")});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,12 +44,6 @@ var resourceRouter = require('./routes/resource');
 
 var app = express();
 
-const connectionString =  process.env.MONGO_CON 
-mongoose = require('mongoose'); 
-mongoose.connect(connectionString,  {useNewUrlParser: true, useUnifiedTopology: true});
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -30,8 +52,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+}));
+app.use(passport.initialize()); 
+app.use(passport.session());
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,16 +69,16 @@ app.use('/addmods', addmodsRouter);
 app.use('/selector', selctorRouter);
 app.use('/resource', resourceRouter);
 
-//Get the default connection 
-var db = mongoose.connection; 
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
  
-//Bind connection to error event  
-db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
-db.once("open", function(){ 
-  console.log("Connection to DB succeeded")
-  }); 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser());
 
-  // We can seed the collection if needed on server start 
+// We can seed the collection if needed on server start 
 async function recreateDB(){ 
   // Delete everything 
   await whiskey.deleteMany(); 
@@ -73,6 +102,7 @@ async function recreateDB(){
  
 let reseed = true; 
 if (reseed) { recreateDB();}
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
