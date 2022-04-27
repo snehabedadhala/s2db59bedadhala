846aef92d06cfdd17da8a5852beb8579b1bcf4bc
snehabedadhala <snehareddy2529@gmail.com>
2022-04-27 18:35:26 -0500

    Added Bonus

--- models/whiskey.js
+++ models/whiskey.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose") 
 const whiskeySchema = mongoose.Schema({ 
  name: String, 
- whiskey_type: String,
+ whiskey_type: {
+     type:String,
+     minLength:1,
+     maxlength:15
+ },
  price: {
     type: Number,
     min: 0,
