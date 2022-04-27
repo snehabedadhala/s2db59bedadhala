7eb5f9bd529895a715e83ebc4502c4aada42d097
snehabedadhala <snehareddy2529@gmail.com>
2022-04-27 17:58:56 -0500

    Added Buttons

--- models/whiskey.js
+++ models/whiskey.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose") 
 const whiskeySchema = mongoose.Schema({ 
  name: String, 
- whiskey_type: String, 
- price: Number 
+ whiskey_type: String,
+ price: {
+    type: Number,
+    min: 0,
+    max: 300} 
 }) 
  
 module.exports = mongoose.model("whiskey", whiskeySchema) 
--- routes/whiskey.js
+++ routes/whiskey.js
@@ -6,6 +6,14 @@ var router = express.Router();
 router.get('/', whiskey_controlers.whiskey_view_all_Page ); 
 module.exports = router; 
 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET detail costume page */ 
 router.get('/detail', whiskey_controlers.whiskey_view_one_Page); 
 
@@ -13,7 +21,11 @@ router.get('/detail', whiskey_controlers.whiskey_view_one_Page);
 router.get('/create', whiskey_controlers.whiskey_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', whiskey_controlers.whiskey_update_Page); 
+router.get('/update', secured, whiskey_controlers.whiskey_update_Page); 
 
 /* GET delete costume page */ 
 router.get('/delete', whiskey_controlers.whiskey_delete_Page); 
+
+// A little function to check if we have an authorized user and continue on 
+// or
+// redirect to login. 
